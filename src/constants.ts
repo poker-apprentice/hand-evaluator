cdfe8d409418e4080b4471c0e0cbb1c5df541e8e
Matt Huggins <matt.huggins@gmail.com>
2023-10-08 21:25:43 -0500

    chore: improve type definition for rankOrder

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,10 @@
-export const rankOrder = '23456789TJQKA';
+import { ALL_RANKS } from '@poker-apprentice/types';
+
+// Constructs a string containing every possible value of a string union.
+type Concat<T extends string[]> = T extends [infer F extends string, ...infer R extends string[]]
+  ? `${F}${Concat<R>}`
+  : '';
+
+const join = <T extends string[]>(strings: T): Concat<T> => strings.join('') as Concat<T>;
+
+export const rankOrder = join(ALL_RANKS);
